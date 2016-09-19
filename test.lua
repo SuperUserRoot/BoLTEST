cd66a9742773607c94d4405e4d74a6cb4dab6c0a
SuperUserRoot <thcler@yahoo.de>
2016-09-20 00:47:51 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,13 +1,17 @@
 if myHero.charName ~= "Graves" then return end
-Config = scriptConfig("FreeElo", "output", false)
+Config = scriptConfig("FreeElo", "output", true)
+
+
+if unit and unit.isme then
+	return
+end
 
 function OnLoad()
-Config:addParam("Inject", "Inject?", SCRIPT_PARAM_ONOFF, false)
+	Config:addParam("Inject", "Inject?", SCRIPT_PARAM_ONOFF, false)
 end
 
 function OnProcessAttack(unit, attack)
-	if unit.isMe and animation:lower():find("attack") and Config.Item("inject") then
+	if unit.isMe and spell.name:lower():find("attack") and Config.Item("inject") then
 		CastSpell(_E, mousePos)
 		end	
 	end
-
