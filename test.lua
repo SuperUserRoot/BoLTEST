39d067c4eea84c18daaca8de73e006e007ecc593
SuperUserRoot <thcler@yahoo.de>
2016-09-20 01:09:02 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,11 +1,6 @@
 if myHero.charName ~= "Graves" then return end
 Config = scriptConfig("FreeElo", "output", true)
 
-
-if unit and unit.isme then
-	return
-end
-
 function OnLoad()
 	Config:addParam("Inject", "Inject?", SCRIPT_PARAM_ONOFF, false)
 end
